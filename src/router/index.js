99280614b55edded1e8fb2b3669d4b06e42f21c1
Vinícius S. Oliveira <vinicius.santos.oliv3ira@gmail.com>
2023-03-06 16:23:42 -0300

    auth views

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,20 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from "@/views/home/HomeView.vue";
 import MySupports from "@/views/supports/MySupports.vue";
 import ModulesAndLessons from "@/views/modules/ModulesAndLessons.vue";
+import Auth from "@/views/auth/Auth.vue";
+import ForgetPassword from "@/views/auth/ForgetPassword.vue";
 
 const routes = [
+  {
+    path: '/',
+    name: 'auth',
+    component: Auth
+  },
+  {
+    path: '/recuperar-senha',
+    name: 'forget.password',
+    component: ForgetPassword
+  },
   {
     path: '/campus',
     component: () => import('@/layouts/DefaultTemplate.vue'),
