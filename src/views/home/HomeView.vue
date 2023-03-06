b1042bbe4bb99763a51b41efeec2900a909059be
Vinícius S. Oliveira <vinicius.santos.oliv3ira@gmail.com>
2023-03-06 14:54:56 -0300

    list courses view

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,11 +1,32 @@
 <template>
-  <div class="home">
-    Listagem dos Cursos
+
+  <div class="pageTitle">
+    <span class="title">Meus Cursos</span>
+    <span class="dots">
+      <span></span>
+      <span></span>
+      <span></span>
+    </span>
+  </div>
+
+  <div class="content">
+    <ul class="listCourses">
+      <CardCourse
+        :title="'Curso de Laravel com Vue.js'"
+        :image="''"
+        :bg_name="'laravel'"
+      />
+    </ul>
   </div>
 </template>
 
 <script>
+  import CardCourse from "@/views/home/components/CardCourse.vue";
+
   export default {
-    name: 'HomeView'
+    name: 'HomeView',
+    components: {
+      CardCourse
+    }
   }
 </script>
---
+++ src/views/home/components/CardCourse.vue
@@ -0,0 +1,33 @@
+<template>
+  <li>
+        <span :class="['card', 'bg-' + bg_name]">
+          <span class="icon">
+            <img :src="[ image != '' ? image : require('@/assets/images/icons/default.png') ]" :alt="title">
+          </span>
+          <span class="title">{{ title }}</span>
+          <span class="action">
+            <a href="trainings.html" :class="['btn', bg_name]">Acessar</a>
+          </span>
+        </span>
+    <span class="dots">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "CardCourse",
+  props: [
+      'title',
+      'image',
+      'bg_name'
+  ]
+}
+</script>
+
+<style scoped>
+
+</style>
