db5eb9ed834e5047318c85dc161d7df3eaf2fac8
Vinícius S Oliveira <vinicius.santos.oliv3ira@gmail.com>
2023-03-07 09:09:55 -0300

    config user store

---
+++ src/store/modules/users/index.js
@@ -0,0 +1,27 @@
+export default {
+    state: {
+        user: {
+            name: '',
+            email: '',
+        },
+        loggedIn: false,
+    },
+
+    mutations: {
+        SET_USER (state, user) {
+            state.user = user
+            state.loggedIn = true
+        },
+        LOGOUT (state) {
+            state.user = {
+                name: '',
+                email: ''
+            }
+
+            state.loggedIn = false
+        }
+    },
+
+    actions: {
+    }
+}
