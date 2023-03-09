7b682d254ddbe9821487264de70dbd1ced507673
Vinícius S Oliveira <vinicius.santos.oliv3ira@gmail.com>
2023-03-09 10:21:57 -0300

    apply middleware in the routes

--- src/router/index.js
+++ src/router/index.js
@@ -5,50 +5,72 @@ import ModulesAndLessons from "@/views/modules/ModulesAndLessons.vue";
 import Auth from "@/views/auth/Auth.vue";
 import ForgotPassword from "@/views/auth/ForgotPassword.vue";
 import ResetPassword from "@/views/auth/ResetPassword.vue";
+import { TOKEN_NAME } from '@/config';
+import store from '@/store';
 
 const routes = [
-  {
-    path: '/',
-    name: 'auth',
-    component: Auth
-  },
-  {
-    path: '/recuperar-senha',
-    name: 'forgot.password',
-    component: ForgotPassword
-  },
-  {
-    path: '/reset/:token',
-    name: 'reset.password',
-    component: ResetPassword,
-    props: true,
-  },
-  {
-    path: '/campus',
-    component: () => import('@/layouts/DefaultTemplate.vue'),
-    children: [
-      {
-        path: 'modulos',
-        name: 'campus.modules',
-        component: ModulesAndLessons
-      },
-      {
-        path: 'minhas-duvidas',
-        name: 'campus.my.supports',
-        component: MySupports
-      },
-      {
-        path: '',
-        name: 'campus.home',
-        component: HomeView
-      }
-    ]
-  }
+    {
+        path: '/',
+        name: 'auth',
+        component: Auth
+    },
+    {
+        path: '/recuperar-senha',
+        name: 'forgot.password',
+        component: ForgotPassword
+    },
+    {
+        path: '/reset/:token',
+        name: 'reset.password',
+        component: ResetPassword,
+        props: true,
+    },
+    {
+        path: '/campus',
+        component: () => import('@/layouts/DefaultTemplate.vue'),
+        children: [
+            {
+                path: 'modulos',
+                name: 'campus.modules',
+                component: ModulesAndLessons
+            },
+            {
+                path: 'minhas-duvidas',
+                name: 'campus.my.supports',
+                component: MySupports
+            },
+            {
+                path: '',
+                name: 'campus.home',
+                component: HomeView
+            }
+        ]
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+
+router.beforeEach(async (to, _, next) => {
+    const loggedIn = store.state.users.loggedIn
+
+    if (to.name != 'reset.password' && !loggedIn) {
+
+        const token = await localStorage.getItem(TOKEN_NAME)
+
+        if (!token && to.name != 'auth' && to.name != 'forgot.password') {
+            return router.push({name: 'auth'})
+        }
+
+        await store.dispatch('getUserAuth')
+            .catch(() => {
+                if (to.name != 'auth') return router.push({name: 'auth'})
+            })
+    }
+
+    next()
 })
 
 export default router
